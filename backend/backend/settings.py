ca7aaa38bbd1e7c686315950cb4a816bedaf53af
Ronak Jain <95034420+jainr5668@users.noreply.github.com>
2023-02-20 19:08:44 +0530

    changed DEBUG flag

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--gj+=^n-nm&8um%_m!g9u=77lz0!s72my-@b9)4zbma8w!&@q('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
