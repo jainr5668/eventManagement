a6afb898fc65ccd714d1ccd9055ab03e7ff3307d
Ronak Jain <95034420+jainr5668@users.noreply.github.com>
2023-05-31 23:02:46 +0530

    add_runner

---
+++ ews/src/app/features/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'feature-sidebar',
+    templateUrl: './sidebar.component.html',
+  //   styleUrls: ['./app.component.css'],
+  })
+export class SidebarComponent{}
